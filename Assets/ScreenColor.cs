597164cc04a4c23d8bfdae90ef60e65700de76d6
RapidDev <kontakt@rapiddev.pl>
2020-01-08 13:20:00 +0100

    Init

---
+++ Assets/ScreenColor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Aurora.Assets
+{
+    class ScreenColor
+    {
+        Bitmap bitmap_screen;
+        Graphics graphic_screen;
+
+        int screenWidth = 0;
+        int screenHeight = 0;
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+
+        public ScreenColor()
+        {
+            screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            screenHeight = Screen.PrimaryScreen.Bounds.Height;
+
+            this.takeScreenshot();
+        }
+
+        public void Refresh()
+        {
+            this.takeScreenshot();
+        }
+
+        public System.Drawing.Color getPixel(int x = 1, int y = 1)
+        {
+            return bitmap_screen.GetPixel(x, y);
+        }
+
+        public ImageSource getPreview()
+        {
+            IntPtr handle = bitmap_screen.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
+        }
+
+        private void takeScreenshot()
+        {
+            bitmap_screen = new Bitmap(screenWidth, screenHeight, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            graphic_screen = Graphics.FromImage(bitmap_screen);
+            graphic_screen.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+            
+            MemoryStream msIn = new MemoryStream();
+            bitmap_screen.Save(msIn, System.Drawing.Imaging.ImageCodecInfo.GetImageEncoders()[0], null);
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using Aurora.Assets;
+
+namespace Aurora
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int tickRate = 200;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            AsyncScreenshot();
+
+            scWidth.Text = Screen.PrimaryScreen.Bounds.Width.ToString();
+            scHeight.Text = Screen.PrimaryScreen.Bounds.Height.ToString();
+        }
+
+        private void AsyncScreenshot()
+        {
+            int counter = 0;
+            ScreenColor screenSource = new ScreenColor();
+            _ = Task.Factory.StartNew(() =>
+              {
+                  Action refreshAction = delegate
+                  {
+                      screenSource.Refresh();
+                      counter++;
+                  };
+                  while (true)
+                  {
+                      System.Windows.Application.Current.Dispatcher.Invoke(DispatcherPriority.Normal, refreshAction);
+                      this.Dispatcher.Invoke(() =>
+                      {
+                          scTest.Text = "Thread started, counter = " + counter;
+
+                          System.Drawing.Color pixel = screenSource.getPixel(100, 100);
+                          System.Drawing.Color pixel2 = screenSource.getPixel(500, 500);
+
+                          scWidth.Text = "Pixel from 100 x 100 have color: R:" + pixel.R + ", G: " + pixel.G + ", B: " + pixel.B;
+                          scHeight.Text = "Pixel from 500 x 500 have color: R:" + pixel2.R + ", G: " + pixel2.G + ", B: " + pixel2.B;
+                      });
+                      System.Threading.Thread.Sleep(tickRate);
+                  }
+              }
+            );
+        }
+    }
+}
